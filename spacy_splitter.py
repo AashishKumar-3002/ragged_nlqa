9c94f9ef30f0fdf013b8fd00ba85504d3901d802
AashishKumar-3002 <aashish201810kumar@gmail.com>
2024-06-17 17:59:17 +0530

    document loader and splitter ready

---
+++ spacy_splitter.py
@@ -0,0 +1,96 @@
+import json
+import copy
+from typing import Iterable, List, Optional
+import spacy.cli
+from langchain.text_splitter import SpacyTextSplitter
+from langchain_core.documents import Document
+
+class UniSplitter:
+    def __init__(
+        self,
+        chunk_size: Optional[int] = None,
+        chunk_overlap: Optional[int] = None,
+        add_start_index: bool = False,
+        use_sizeoverlap: Optional[bool] = False,
+        allow_chunk_oversize: Optional[bool] = False
+    ) -> None:
+        """Initialize with chunk size and overlap."""
+        self.chunk_size = int(chunk_size) if chunk_size is not None else None
+        self.chunk_overlap = int(chunk_overlap) if chunk_overlap is not None else None
+        self.use_sizeoverlap = use_sizeoverlap
+        self._add_start_index = add_start_index
+        self._allow_chunk_oversize = allow_chunk_oversize
+
+    def split_text_in_json(self, texts: List[str], metadatas: Optional[List[dict]] = None, model_name: str = 'en_core_web_md') -> List[Document]:
+        """
+        Splits the text in the JSON file into chunks using SpacyTextSplitter and saves the result.
+        
+        Parameters:
+            model_name (str): The name of the Spacy model to use. Defaults to 'en_core_web_md'.
+        """
+        # Download the Spacy model if not available
+        spacy.cli.download(model_name)
+        try:
+            # Initialize the SpacyTextSplitter
+            if self.use_sizeoverlap and self.chunk_size and self.chunk_overlap and self._allow_chunk_oversize:
+                text_splitter = SpacyTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap, pipeline=model_name)
+            else:
+                text_splitter = SpacyTextSplitter(pipeline=model_name)
+            
+            # Process each item in the json data
+            _metadatas = metadatas or [{}] * len(texts)
+            all_texts = []
+            for i, text in enumerate(texts):
+                index = 0
+                previous_chunk_len = 0
+                chunks = text_splitter.split_text(text)
+
+                # Further split chunks if they exceed the chunk_size
+                if self.chunk_size and not self._allow_chunk_oversize and self.use_sizeoverlap:
+                    chunks = self._split_large_chunks(chunks)
+
+                for chunk in chunks:
+                    metadata = copy.deepcopy(_metadatas[i])
+                    if self._add_start_index:
+                        offset = index + previous_chunk_len - (self.chunk_overlap if self.chunk_overlap else 0)
+                        index = text.find(chunk, max(0, offset))
+                        metadata['start_index'] = index
+                        previous_chunk_len = len(chunk)
+                    new_doc = Document(page_content=chunk, metadata=metadata)
+                    all_texts.append(new_doc)
+            
+            print(f"Processed {len(all_texts)} items.")
+            return all_texts
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None
+    
+    def _split_large_chunks(self, chunks: List[str]) -> List[str]:
+        """Split large chunks into smaller ones based on the specified chunk size."""
+        split_chunks = []
+        for chunk in chunks:
+            if len(chunk) > self.chunk_size:
+                start = 0
+                while start < len(chunk):
+                    end = start + self.chunk_size
+                    split_chunks.append(chunk[start:end])
+                    start += self.chunk_size - (self.chunk_overlap if self.chunk_overlap else 0)
+            else:
+                split_chunks.append(chunk)
+        return split_chunks
+
+    def split_spacy_documents(self, documents: Iterable[Document] ,  model_name: Optional[str] = None) -> List[Document]:
+        """split the spacy documents into chunks
+        
+        Parameters:
+            documents (list): A list of Document objects containing page content and metadata.
+        """
+        texts, metadatas = [], []
+        for doc in documents:
+            texts.append(doc.page_content)
+            metadatas.append(doc.metadata)
+        
+        if model_name:
+            return self.split_text_in_json(texts, metadatas, model_name)
+        
+        return self.split_text_in_json(texts, metadatas)
---
+++ uni_chunker.py
@@ -0,0 +1,125 @@
+import logging
+from typing import List, Optional
+from langchain.docstore.document import Document
+from langchain.document_loaders.base import BaseLoader
+from langchain_community.document_loaders.helpers import detect_file_encodings
+from spacy_splitter import UniSplitter
+import json
+
+logger = logging.getLogger(__name__)
+
+class UniTextLoader(BaseLoader):
+    def __init__(self, file_path: str, encoding: Optional[str] = None, autodetect_encoding: bool = False):
+        self.file_path = file_path
+        self.encoding = encoding
+        self.autodetect_encoding = autodetect_encoding
+
+    def load(self) -> List[Document]:
+        documents = []
+        try:
+            with open(self.file_path, "r", encoding=self.encoding) as f:
+                data = json.load(f)
+                for file_data in data:
+                    title = file_data['title']
+                    text = file_data['text']
+                    author = file_data.get('author', '')
+                    hostname = file_data.get('hostname', '')
+                    date = file_data.get('date', '')
+                    fingerprint = file_data.get('fingerprint', '')
+                    source = file_data.get('source', '')
+                    source_hostname = file_data.get('source-hostname', '')
+                    excerpt = file_data.get('excerpt', '')
+                    sublinks = file_data.get('metadata', '')
+
+                    metadata = {
+                        "title": title,
+                        "author": author,
+                        "hostname": hostname,
+                        "date": date,
+                        "fingerprint": fingerprint,
+                        "source": source,
+                        "source_hostname": source_hostname,
+                        "excerpt": excerpt,
+                        "sublinks": sublinks
+                    }
+                    document = Document(page_content=text, metadata=metadata)
+                    documents.append(document)
+        except UnicodeDecodeError as e:
+            if self.autodetect_encoding:
+                detected_encodings = detect_file_encodings(self.file_path)
+                for encoding in detected_encodings:
+                    logger.debug("Trying encoding: ", encoding.encoding)
+                    try:
+                        with open(self.file_path, "r", encoding=encoding.encoding) as f:
+                            data = json.load(f)
+                            for file_data in data:
+                                title = file_data['title']
+                                text = file_data['text']
+                                author = file_data.get('author', '')
+                                hostname = file_data.get('hostname', '')
+                                date = file_data.get('date', '')
+                                fingerprint = file_data.get('fingerprint', '')
+                                source = file_data.get('source', '')
+                                source_hostname = file_data.get('source-hostname', '')
+                                excerpt = file_data.get('excerpt', '')
+                                sublinks = file_data.get('metadata', '')
+
+                                metadata = {
+                                    "title": title,
+                                    "author": author,
+                                    "hostname": hostname,
+                                    "date": date,
+                                    "fingerprint": fingerprint,
+                                    "source": source,
+                                    "source_hostname": source_hostname,
+                                    "excerpt": excerpt,
+                                    "sublinks": sublinks
+                                }
+                                document = Document(page_content=text, metadata=metadata)
+                                documents.append(document)
+                        break
+                    except UnicodeDecodeError:
+                        continue
+            else:
+                raise RuntimeError(f"Error loading {self.file_path}") from e
+        except Exception as e:
+            raise RuntimeError(f"Error loading {self.file_path}") from e
+
+        return documents
+
+
+class UniTextProcessor:
+    def __init__(self, loader: BaseLoader, chunk_size: Optional[int] = None, chunk_overlap: Optional[int] = None, use_sizeoverlap: Optional[bool] = False , add_start_index: Optional[bool] = False , allow_chunk_oversize: Optional[bool] = False) -> None:
+        self.loader = loader
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        self.use_sizeoverlap = use_sizeoverlap
+        self.add_start_index = add_start_index
+        self.allow_chunk_oversize = allow_chunk_oversize
+
+    def spacy_process_documents(self) -> List[str]:
+        documents = self.loader.load()
+        spacy_text_splitter = UniSplitter(
+            chunk_size=self.chunk_size, 
+            chunk_overlap=self.chunk_overlap, 
+            use_sizeoverlap=self.use_sizeoverlap,
+            add_start_index=self.add_start_index,
+            allow_chunk_oversize=self.allow_chunk_oversize
+        )
+        MODEL_NAME = 'en_core_web_md'
+        chunks_store = []
+        for doc in documents:
+            new_doc = [Document(page_content=doc.page_content, metadata=doc.metadata)]
+            doc_text = spacy_text_splitter.split_spacy_documents(new_doc , model_name=MODEL_NAME)
+            chunks_store.extend(doc_text)
+        return chunks_store
+
+# Usage example:
+loader = UniTextLoader("final_result.json", encoding="utf-8", autodetect_encoding=True)
+text_processor = UniTextProcessor(loader, use_sizeoverlap=False, chunk_size=1000, chunk_overlap=100 , add_start_index=True , allow_chunk_oversize=False)
+processed_chunks = text_processor.spacy_process_documents()
+
+print(processed_chunks)
+print(len(processed_chunks))
+print([d.page_content for d in processed_chunks])
+print([d.metadata for d in processed_chunks])
