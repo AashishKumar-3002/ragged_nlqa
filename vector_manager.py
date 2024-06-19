6a7973dc17e84128d2ccba9e86783f6a547489f7
AashishKumar-3002 <aashish201810kumar@gmail.com>
2024-06-20 05:02:51 +0530

    vector upsert successfull

---
+++ vector_manager.py
@@ -0,0 +1,43 @@
+from langchain_qdrant import Qdrant
+from qdrant_client import QdrantClient
+from langchain_huggingface import HuggingFaceEmbeddings
+import json
+
+from uni_chunker import UniTextLoader, UniTextProcessor
+
+class VectorManager:
+    def __init__(self, path: str, collection_name: str, model_name: str , url: str = None , api_key: str = None):
+        self.embeddings = HuggingFaceEmbeddings(
+            model_name=model_name,
+        )
+        self.url = url
+        self.api_key = api_key
+        self.path = path
+        self.collection_name = collection_name
+
+    def upsert_documents(self, documents):
+
+        qdrant = Qdrant.from_documents(
+            documents,
+            self.embeddings,
+            url=self.url,
+            api_key=self.api_key,
+            path=self.path,
+            collection_name=self.collection_name,
+        )
+
+        return qdrant
+
+if __name__ == "__main__":
+    model_name="sentence-transformers/all-mpnet-base-v2"
+    path = "./db/standford_embedding"
+    collection_name = "Standford_embedding"
+    vector_manager = VectorManager(path=path, collection_name=collection_name, model_name=model_name)
+
+    loader = UniTextLoader("final_result.json", encoding="utf-8", autodetect_encoding=True)
+    text_processor = UniTextProcessor(loader, use_sizeoverlap=False, chunk_size=1000, chunk_overlap=100 , add_start_index=False , allow_chunk_oversize=False)
+    processed_chunks = text_processor.spacy_process_documents()
+    
+    qdrant = vector_manager.upsert_documents(processed_chunks)
+
+    print(qdrant)
