4c86b7369fb200b1db044e4aeac8a4ee7e053d22
AashishKumar-3002 <aashish201810kumar@gmail.com>
2024-06-23 17:01:24 +0530

    added chatmodel callling

--- ragged_nlqs/retrieval/staging_retrival.py
+++ ragged_nlqs/retrieval/staging_retrival.py
@@ -5,9 +5,10 @@ from qdrant_client.http import models as rest
 from ragged_nlqs.retrieval.reranking import ranking_passage_formatter
 from ragged_nlqs.retrieval.vector_search import VectorSearch
 from ragged_nlqs.retrieval.token_counter import get_token_count
+from ragged_nlqs.retrieval.chat_model import ChatModel
 
 
-def process_retrival(db_path ,collection_name, output_retrival_path, query , k=7 , filter_params="metadata.title", title_filters=None , reranking_model="nano", chat_model_type=None):
+def process_retrival(db_path ,collection_name, output_retrival_path, query , k=7 , filter_params="metadata.title", title_filters=None , reranking_model="nano", chat_model_type=None, api_key="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"):
     search = VectorSearch(path=db_path , collection_name=collection_name)
     filter_values = []
     for title_filter in title_filters:
@@ -36,12 +37,30 @@ def process_retrival(db_path ,collection_name, output_retrival_path, query , k=7
     len_text_sublinks = len(text_sublinks)
     if chat_model_type == "hf":
         if get_token_count(text_sublinks) > 9516:
-            text_sublinks = text_sublinks[:int(len_text_sublinks-1)-1] 
+            text_sublinks = text_sublinks[:int(len_text_sublinks-1)-1]
     elif chat_model_type == "groq":
         if get_token_count(text_sublinks) > 7168:
             text_sublinks = text_sublinks[:int(len_text_sublinks-1)-1]
     elif chat_model_type == "replicate":
         if get_token_count(text_sublinks) > 9516:
             text_sublinks = text_sublinks[:int(len_text_sublinks-1)-1]
+    else:
+        raise ValueError("Invalid chat model type. Please select one of the following: hf, groq, replicate")
+    
+    if chat_model_type == "hf":
+        context = str(text_sublinks)
+        # Call the hf chat model
+        ChatModel(model_name="mistralai/Mistral-7B-Instruct-v0.2", api_key=api_key)
+        ChatModel.HuggingFaceInferenceClient(context=context , query=query)
+    elif chat_model_type == "groq":
+        context = str(text_sublinks)
+        # Call the groq chat model
+        ChatModel(model_name="llama3-8b-8192", api_key=api_key)
+        ChatModel.GroqClientLlm(context=context , query=query)
+    elif chat_model_type == "replicate":
+        context = str(text_sublinks)
+        # Call the replicate chat model
+        ChatModel(model_name="mistralai/mixtral-8x7b-instruct-v0.1", api_key=api_key)
+        ChatModel.replicateChatLlm(context=context , query=query)
     else:
         raise ValueError("Invalid chat model type. Please select one of the following: hf, groq, replicate")
