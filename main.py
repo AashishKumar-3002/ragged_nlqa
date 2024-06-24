112aafb2feb9bae99e7473f8b9893413706898fe
AashishKumar-3002 <aashish201810kumar@gmail.com>
2024-06-24 19:07:35 +0530

    try this

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
-
+from ragged_nlqs.ingestion.staging_ingestion import process_ingest
+from ragged_nlqs.retrieval.staging_retrival import process_retrival
 import json
-from ragged_nlqs.ingestion.uni_chunker import UniTextLoader, UniTextProcessor
-from ragged_nlqs.ingestion.uni_crawler import UniCrawler
-from ragged_nlqs.ingestion.vector_manager import VectorManager
-from ragged_nlqs.retrieval.reranking import ranking_passage_formatter
-from ragged_nlqs.retrieval.vector_search import VectorSearch
-from qdrant_client.http import models as rest
 
-query = "what is the main difference between langchain and llamaindex?"
-reranked_passages = ranking_passage_formatter("result.json" , query, type_model="nano")
-print(reranked_passages)
+def main():
+    # Ingest data
+    process_ingest(db_path="data.db", collection_name="data", data_path="data.json")
+
+    # Retrieve data
+    query = "What is the capital of France?"
+    title_filters = ["capital of France"]
+    process_retrival(db_path="data.db", collection_name="data", output_retrival_path="retrieval_results.json", query=query, title_filters=title_filters)
